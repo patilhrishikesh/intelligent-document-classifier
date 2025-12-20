f48d0e7066f38040e8bca2d249f1ae22e0cbecb3
Hrishikesh Patil <156280453+patilhrishikesh@users.noreply.github.com>
2025-12-20 22:58:13 +0530

    Remove redundant ARTIFACTS_DIR initialization

--- src/document_classifier/model_persistence.py
+++ src/document_classifier/model_persistence.py
@@ -1,15 +1,11 @@
 import joblib
 from pathlib import Path
 
-<<<<<<< HEAD
 ARTIFACTS_DIR =  Path(__file__).resolve().parents[2] / "artifacts"
 ARTIFACTS_DIR.mkdir(exist_ok = True)
-=======
 # Absolute, OS-independent path to project-root/artifacts
 ARTIFACTS_DIR = Path(__file__).resolve().parents[2] / "artifacts"
 ARTIFACTS_DIR.mkdir(exist_ok=True)
-
->>>>>>> 3bf4e96538d915689f74871a750f1ade31bfdf7e
 
 def save_artifact(obj, filename: str) -> None:
     path = ARTIFACTS_DIR / filename
